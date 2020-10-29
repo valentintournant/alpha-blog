198a8a3ad81c62fa0361d8c700603808a8562736
Valentin Tournant <valentin.tournant@gmail.com>
2020-10-29 15:47:22 +0100

    add flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
   def show
+    # byebug
     @article = Article.find(params[:id])
   end
 
@@ -8,5 +9,24 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
 
+  def create
+    # render plain: params[:article](debug)
+    @article = Article.new(article_params)
+    # render plain: @article.inspect
+    if @article.save
+      redirect_to (@article), notice: 'Article was created successfully.'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
