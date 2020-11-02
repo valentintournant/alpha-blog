27a5afac828f5beafdddf479db2ecbdfef268ee7
Valentin Tournant <valentin.tournant@gmail.com>
2020-11-02 15:24:38 +0100

    validation usr conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
   before_save { self.email = email.downcase }
-=======
->>>>>>> user-articles-association
   has_many :articles
   validates :username, presence:true,
                       uniqueness: { case_sensitive: false },
