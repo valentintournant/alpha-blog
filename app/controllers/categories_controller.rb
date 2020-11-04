fb4c0ec281d94c2af6dafc982af288e3700f447f
Valentin Tournant <valentin.tournant@gmail.com>
2020-11-04 15:08:17 +0100

    Admin user requirement and test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
   def new
     @category = Category.new
@@ -28,4 +29,10 @@ class CategoriesController < ApplicationController
     params.require(:category).permit(:name)
   end
 
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform this action"
+      redirect_to categories_path
+    end
+  end
 end
--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @admin_user = User.create(username: "Jonh Doe", email: "johndoe@exemple.com",
+                              password: "password", admin: true)
+  sign_in_as (@admin_user)
+  end
+
   test "get new category form and create new category" do
     get "/categories/new"
     assert_response :success
